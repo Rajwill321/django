5cd9d5f9ffd3dd77659f48c36cc4cd11e4afa325
Raj Will <rajwill321@outlook.com>
2019-11-26 09:44:52 +0000

    Allowing all hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%ymny-!plsi=7pq)lqqwh-8+(q9(33z-4ut_d4uw(*o2^syadc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.126.207.57']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
